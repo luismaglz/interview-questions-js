833b7e108f3874fcabad0d9a82e04551a9fc7709
luismaglz <luismaglz@live.com>
2022-09-16 14:20:06 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-1xgf3x
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-1xgf3x)
---
+++ index.js
@@ -0,0 +1,54 @@
+const axios = require('axios');
+
+// Import stylesheets
+import './style.css';
+
+// Shape returned by IMDB
+// interface IMovie {
+//   title: string;
+//   description: string;
+//   image: string;
+//   id: string;
+//   resultType: string;
+// }
+
+// interface IMDB_Response {
+//   errorMessage: string;
+//   expression: string;
+//   results: IMovie[];
+// }
+
+async function fetchData(htmlElement, ev) {
+  const input = document.getElementById('data-input');
+  const inputValue = input.value;
+  // endpoint
+  const IMDB_API_SEARCH = 'https://imdb-api.com/en/API/SearchMovie/k_d3adyy0g/';
+
+  const response = await axios.get();
+
+  console.log(response);
+  // createAndRenderTable(response.data.results);
+}
+
+function createAndRenderTable(movies) {
+  const tableContainer = document.getElementById('create-table-here');
+
+  // CREATE YOUR TABLE
+  // tableContainer.innerHTML = table;
+}
+
+function initialize() {
+  const buttonElement = document.getElementById('fetch-data-button');
+  buttonElement.addEventListener('click', fetchData);
+}
+
+// Write TypeScript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `
+<button id="fetch-data-button">Fetch Data</button><input id="data-input" placeholder="movie title goes here"></input>
+<div id="create-table-here">
+create your table here
+</div>
+`;
+
+setTimeout(initialize, 10);
